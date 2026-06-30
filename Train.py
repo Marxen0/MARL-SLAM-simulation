b998a3aa99aaa0971452601527f3561176a321cd
Marxen0 <94890277+Marxen0@users.noreply.github.com>
2026-06-30 22:28:10 +0700

    data log

--- Train.py
+++ Train.py
@@ -5,10 +5,8 @@ import torch.optim as optim
 import Environment
 from helpers.map_generator import OccupancyViewer
 from helpers.frontier_finder import get_frontiers
-import pandas as pd
-import os
+import csv
 
-filename = "training_log.xlsx"
 # Import the environment class we just designed
 # from your_env_file import environment
 
@@ -40,14 +38,14 @@ def train_marl_async():
     RAY_COUNT = 16
     WORLD_WIDTH = 50
     WORLD_HEIGHT = 50
-    EPISODES = 10
+    EPISODES = 500
     
     env = Environment.environment(AGENT_NUM, RAY_COUNT, WORLD_WIDTH, WORLD_HEIGHT)
-    viewer = OccupancyViewer(
-        env.world_widht,
-        env.world_height,
-        cell_size=20
-    )
+   # viewer = OccupancyViewer(
+   #     env.world_widht,
+   #     env.world_height,
+   #     cell_size=20
+   # )
     # Initialize a policy and optimizer for the agents
     # (Shared policy layout common in Parameter Sharing MARL)
     policy = MultiAgentPolicy()
@@ -92,12 +90,11 @@ def train_marl_async():
             # 3. Environment Step Phase
             # The env will process the new actions for idle agents, progress the clock,
             # and pause as soon as AT LEAST ONE agent reaches its destination.
-            viewer.render(
-                env.global_map,
-                env.ag_pos,
-            )
+    #        viewer.render(
+    #            env.global_map,
+    #            env.ag_pos,
+    #        )
             next_observations, rewards, done = env.step(actions)
-
             
             # Track rewards accumulated during this step interval
             episode_reward += np.sum(rewards)
@@ -110,9 +107,13 @@ def train_marl_async():
             # Advance state forward
             observations = next_observations
 
+        with open("training_log.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([episode, rewards, env.time])
         # Logging metrics at the end of every episode
         if episode % 10 == 0:
             print(f"Episode {episode:03d} | Total Step Penalty (Reward): {episode_reward:.1f} | Env Simulation Clock: {env.time} steps")
+        
 
 if __name__ == "__main__":
     # To run this script seamlessly, copy the updated environment class from the previous 
